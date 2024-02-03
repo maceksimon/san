8bb6b8d4fc0b44ecdc08d9860acccefe63ebc436
Šimon Macek <simon.macek@proficio.cz>
2024-02-03 12:21:31 -0500

    layout fix

--- components/FormNewsletter.vue
+++ components/FormNewsletter.vue
@@ -6,6 +6,8 @@
         {{ ' ' }}
         <p class="inline sm:block">Napište nám.</p>
       </div>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <form
         netlify
         name="Registrační formulář"
@@ -13,24 +15,27 @@
         action="/success"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        class="mt-10 max-w-md space-y-8"
+        class="mt-10 grid md:grid-cols-2 gap-y-8 gap-x-16"
       >
-        <!-- Form fields -->
-        <FormRadioRegistrar />
-        <FormName />
-        <FormTextareaComment />
-        <FormCheckboxNewsletter />
-        <!-- Signup -->
-        <div class="flex gap-x-4">
-          <label for="email-address" class="sr-only">E-mail</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6" placeholder="Zadejte svůj e-mail" />
-          <button type="submit" class="flex-none rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600">Zapsat se</button>
+        <div>
+          <FormRadioRegistrar />
         </div>
-        <p class="mt-4 text-sm leading-6 text-gray-900">Jak vaše data zpracováváme? Přečtěte si naše <a href="#" class="font-semibold text-amber-600 hover:text-amber-500">podmínky zpracování osobních údajů</a>.</p>
-        <input type="hidden" name="form-name" value="Registrační formulář" />
-        <p hidden class="sr-only  ">
-          <label>Don’t fill this out: <input name="bot-field" /></label>
-        </p>
+        <div class="space-y-8">
+          <FormName />
+          <FormTextareaComment />
+          <FormCheckboxNewsletter />
+          <!-- Signup -->
+          <div class="flex gap-x-4">
+            <label for="email-address" class="sr-only">E-mail</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6" placeholder="Zadejte svůj e-mail" />
+            <button type="submit" class="flex-none rounded-md bg-amber-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600">Zapsat se</button>
+          </div>
+          <p class="mt-4 text-sm leading-6 text-gray-900">Jak vaše data zpracováváme? Přečtěte si naše <a href="#" class="font-semibold text-amber-600 hover:text-amber-500">podmínky zpracování osobních údajů</a>.</p>
+          <input type="hidden" name="form-name" value="Registrační formulář" />
+          <p hidden class="sr-only  ">
+            <label>Don’t fill this out: <input name="bot-field" /></label>
+          </p>
+        </div>
       </form>
     </div>
   </div>

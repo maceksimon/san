7e9b97e3a8da12de619c639ed100fe3ad661309b
Šimon Macek <simon.macek@proficio.cz>
2024-02-04 08:04:33 -0500

    color and logo update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import tailwindForms from '@tailwindcss/forms'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -7,8 +8,30 @@ export default defineNuxtConfig({
     exposeConfig: true,
     viewer: true,
     config: {
-      plugins: [tailwindForms]
-    }
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: [...defaultTheme.fontFamily.sans],
+          },
+          colors: {
+            amber: {
+              '50': '#fdf5ef',
+              '100': '#fae8da',
+              '200': '#f5cdb3',
+              '300': '#eeac83',
+              '400': '#e58152',
+              '500': '#df6130',
+              '600': '#d94e29',
+              '700': '#ad3821',
+              '800': '#8b2e21',
+              '900': '#70281e',
+              '950': '#3c120e',
+            },
+          },
+        },
+      },
+      plugins: [tailwindForms],
+    },
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })

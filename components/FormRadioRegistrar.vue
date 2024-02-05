8b09f9cb5719c73dc9a43ad3a93321b274d1bff1
Šimon Macek <simon.macek@proficio.cz>
2024-02-04 21:21:47 -0500

    update registrar values

--- components/FormRadioRegistrar.vue
+++ components/FormRadioRegistrar.vue
@@ -4,10 +4,10 @@
     <fieldset class="mt-4">
       <legend class="sr-only">Kdo jste v saunařském světě?</legend>
       <div class="space-y-4">
-        <div v-for="notificationMethod in notificationMethods" :key="notificationMethod.id" class="flex items-center">
-          <input :id="notificationMethod.id" name="notification-method" type="radio" :checked="notificationMethod.id === 'majitel'" class="h-4 w-4 border-gray-300 text-amber-600 focus:ring-amber-600" />
-          <label :for="notificationMethod.id" class="ml-3 block text-sm font-medium leading-6 text-gray-900">
-            {{ notificationMethod.title }}
+        <div v-for="option in registrarOptions" :key="option.id" class="flex items-center">
+          <input :id="option.id" name="registrar-type" type="radio" :checked="option.id === 'majitel'" :value="option.id" class="h-4 w-4 border-gray-300 text-amber-600 focus:ring-amber-600" />
+          <label :for="option.id" class="ml-3 block text-sm font-medium leading-6 text-gray-900">
+            {{ option.title }}
           </label>
         </div>
       </div>
@@ -16,7 +16,7 @@
 </template>
 
 <script setup>
-const notificationMethods = [
+const registrarOptions = [
   { id: 'majitel', title: 'Majitel/ka potírny' },
   { id: 'planujici', title: 'Člověk s plánem otevřít si saunu' },
   { id: 'saunar', title: 'Hrdý saunař' },

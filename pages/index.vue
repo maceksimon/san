30e44ec3d543c2bf14f5aed13a99fb0de6fda949
Šimon Macek <simon.macek@proficio.cz>
2024-02-12 08:48:30 -0500

    og image + meta

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,14 @@
     <FormNewsletter />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Rozsviť s náma SANN | SANN.cz',
+  ogTitle: 'Rozsviť s náma SANN',
+  description: 'Saunová asociace nezávislých nadšenců (SANN) sdružuje saunáčstvo s divokostí v srdci a majitele nezkrocených potíren.',
+  ogDescription: 'Saunová asociace nezávislých nadšenců (SANN) sdružuje saunáčstvo s divokostí v srdci a majitele nezkrocených potíren.',
+  ogImage: 'https://sann.cz/images/og-image.jpg',
+  twitterCard: 'summary_large_image',
+})
+</script>

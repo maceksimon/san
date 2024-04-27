fcd1ce2b4b557adc6977ffe7a6074c401f9a34ac
Šimon Macek <simon.macek@proficio.cz>
2024-04-27 11:39:31 -0400

    form workshop: rename fields

--- components/FormWorkshop.vue
+++ components/FormWorkshop.vue
@@ -9,8 +9,21 @@
         <div class="prose prose-lg">
           <p>Vyplň formulář, nebo nám napiš na <a href="mailto:ahoj.sann@gmail.com">ahoj.sann@gmail.com</a>.</p>
         </div>
-        <InputFullName />
-        <InputEmail />
+        <div>
+          <label for="fullNameWorkshop" class="block font-medium leading-6 text-gray-900">Celé jméno <span class="text-red-600">*</span></label>
+          <div class="mt-2">
+            <input type="text" name="full_name_workshop" id="fullNameWorkshop"
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6"
+              placeholder="" autocomplete="name" required />
+          </div>
+        </div>
+        <div>
+          <label for="emailWorkshop" class="block font-medium leading-6 text-gray-900">E-mail <span class="text-red-600">*</span></label>
+          <div class="mt-2">
+            <input id="emailWorkshop" name="email_workshop" type="email" autocomplete="email" required
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-amber-600 sm:text-sm sm:leading-6" placeholder="" />
+          </div>
+        </div>
         <!-- Signup -->
         <div class="flex justify-between items-center gap-4">
           <div>
